1d3f9a073f5e6d01af44decf6e4d8df424e83127
Christian <christiandhubbs@gmail.com>
2020-02-26 11:07:55 -0600

    renamed or_gym_envs to envs

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!usr/bin/env python
+
+from setuptools import setup, find_packages
+import sys
+import os
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'or_gym'))
+from version import VERSION
+
+setup(name='or-gym',
+	version=VERSION,
+	description='OR-Gym: A set of environments for developing reinforcement learning agents for OR problems.',
+	author='Christian Hubbs',
+	license='MIT',
+	# pacakges=[package for package in find_packages() if package.startswith('something')],
+	install_requires=['gym>=0.17.0','matplotlib>=3.0.3',
+		'numpy>=1.18.1','scipy>=1.4.1','torch>=1.0.0','torchvision>=0.5.0','pyomo>=5.5'],
+)
