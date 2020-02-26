87c51d3a84bedc9bad051f4202a7028f6366bbaf
Christian <christiandhubbs@gmail.com>
2020-02-26 12:24:37 -0600

    updating setup file

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name='or-gym',
 	author='Christian Hubbs',
 	license='MIT',
 	# pacakges=[package for package in find_packages() if package.startswith('something')],
-	install_requires=['gym>=0.15.0','matplotlib>=3.0.3',
+	install_requires=['gym>=0.15.0','matplotlib>=3.0.3','str2bool>=1',
 		'numpy>=1.16.1','scipy>=1.4.1','torch>=1.4.0','torchvision>=0.5.0','pyomo>=5.5'],
 	zip_safe=False
 )
