83a333a17ad14e227a820effd4afb9c67a4f9502
Christian <christiandhubbs@gmail.com>
2020-02-26 12:35:38 -0600

    added file

--- or_gym/algos/math_prog_utils.py
+++ or_gym/algos/math_prog_utils.py
@@ -4,5 +4,5 @@ from pyomo.opt import SolverFactory
 def solve_math_program(model, solver='glpk', print_results=True):
 
 	solver = SolverFactory(solver)
-	results = solver.solve(m, tee=print_results)
-	return m, results
+	results = solver.solve(model, tee=print_results)
+	return model, results
