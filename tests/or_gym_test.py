4fb097860f0c882e69e33555c37d764828502ec7
Christian <christiandhubbs@gmail.com>
2020-02-26 11:54:05 -0600

    fixed test file

--- tests/or_gym_test.py
+++ tests/or_gym_test.py
@@ -2,6 +2,8 @@ import gym
 import or_gym
 
 env = gym.make('Knapsack-v0')
-
-env.step()
+print('Knapsack-v0 initialized successfully')
+print(env.step(1))
+print('Step successful')
 env.reset()
+print('Reset successful')
