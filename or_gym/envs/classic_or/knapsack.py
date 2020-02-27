d2d18d3e9e39988fd45a40af033f76ed4b79560c
Christian <christiandhubbs@gmail.com>
2020-02-27 11:47:32 -0600

    added online knapsack problem

--- or_gym/envs/classic_or/knapsack.py
+++ or_gym/envs/classic_or/knapsack.py
@@ -22,7 +22,8 @@ class KnapsackEnv(gym.Env):
         Type: Tuple, Discrete
         0: list of item weights
         1: list of item values
-        2: current weight in knapsack
+        2: maximum weight of the knapsack
+        3: current weight in knapsack
 
     Actions:
         Type: Discrete
@@ -53,7 +54,7 @@ class KnapsackEnv(gym.Env):
         self.observation_space = spaces.Tuple((
             spaces.Discrete(self.N),
             spaces.Discrete(self.N),
-            spaces.Discrete(1)))
+            spaces.Discrete(2)))
         
         self.seed()
         self.reset()
@@ -79,9 +80,11 @@ class KnapsackEnv(gym.Env):
         return self.state
     
     def _update_state(self):
-        self.state = (self.item_weights,
-                      self.item_values, 
-                      self.current_weight)
+        self.state = (
+            self.item_weights,
+            self.item_values,
+            self.max_weight,
+            self.current_weight)
     
     def reset(self):
         self.current_weight = 0
@@ -110,7 +113,9 @@ class BoundedKnapsackEnv(KnapsackEnv):
         0: list of item weights
         1: list of item values
         2: list of item limits
-        3: current weight in knapsack
+        3: maximum weight of the knapsack
+        4: current weight in knapsack
+
 
     Actions:
         Type: Discrete
@@ -160,16 +165,83 @@ class BoundedKnapsackEnv(KnapsackEnv):
         if item is not None:
             self.item_limits[item] -= 1
             
-        self.state = (self.item_weights,
-                      self.item_values,
-                      self.item_limits,
-                      self.current_weight)
+        self.state = (
+            self.item_weights,
+            self.item_values,
+            self.item_limits,
+            self.max_weight,
+            self.current_weight)
         
     def sample_action(self):
         return np.random.choice(
             self.item_numbers[np.where(self.item_limits!=0)])
     
     def reset(self):
+        self.current_weight = 0
+        self.item_limits = self.item_limits_init.copy()
+        self._update_state()
+        return self.state
+
+class OnlineKnapsackEnv(BoundedKnapsackEnv):
+    
+    def __init__(self):
+        BoundedKnapsackEnv.__init__(self)
+        self.action_space = spaces.Discrete(2)
+        self.observation_space = spaces.Tuple((
+            spaces.Discrete(self.N),
+            spaces.Discrete(self.N),
+            spaces.Discrete(3)))
+
+        self.step_counter = 0
+        self.step_limit = 5
+        
+        self.seed()
+        self.state = self.reset()
+        
+    def step(self, action):
+        # Check that item will fit
+        if bool(action):
+            # Check that item will fit
+            if self.item_weights[self.current_item] + self.current_weight <= self.max_weight:
+                self.current_weight += self.item_weights[self.current_item]
+                reward = self.item_values[self.current_item]
+                if self.current_weight == self.max_weight:
+                    done = True
+                else:
+                    done = False
+            else:
+                # End if over weight
+                reward = 0
+                done = True
+        else:
+            reward = 0
+            done = False
+            
+        self.step_counter += 1
+        if self.step_counter >= self.step_limit:
+            done = True
+            
+        self._update_state()
+        return self.state, reward, done, {}
+    
+    def _update_state(self):
+        self.current_item = np.random.choice(self.item_numbers, p=self.item_probs)
+        self.state = (
+            self.item_weights,
+            self.item_values,
+            self.item_probs,
+            self.max_weight,
+            np.array([
+                self.current_weight,
+                self.current_item
+            ]))
+        
+    def sample_action(self):
+        return np.random.choice([0, 1])
+    
+    def reset(self):
+    	if not hasattr(self, 'item_probs'):
+    		self.item_probs = self.item_limits_init / self.item_limits_init.sum()
         self.current_weight = 0
         self.item_limits = self.item_limits_init.copy()
         self._update_state()
