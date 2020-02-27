8417d1cb2add5f3103979fd153fa4a83d11a0876
Christian <christiandhubbs@gmail.com>
2020-02-27 11:53:37 -0600

    added import and registration values for the environment

--- or_gym/envs/classic_or/__init__.py
+++ or_gym/envs/classic_or/__init__.py
@@ -1,2 +1,3 @@
 from or_gym.envs.classic_or.knapsack import KnapsackEnv
 from or_gym.envs.classic_or.knapsack import BoundedKnapsackEnv
+from or_gym.envs.classic_or.knapsack import OnlineKnapsackEnv
