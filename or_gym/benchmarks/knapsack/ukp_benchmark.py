c7ce80e700249c2d44457efd7c8b4c5df2bb79ce
Christian <christiandhubbs@gmail.com>
2020-02-28 11:12:13 -0600

    added heuristic to UKP benchmark

--- or_gym/benchmarks/knapsack/ukp_benchmark.py
+++ or_gym/benchmarks/knapsack/ukp_benchmark.py
@@ -1,7 +1,10 @@
 #!usr/bin/env python
 
 from or_gym.algos.knapsack.math_prog import *
+from or_gym.algos.knapsack.heuristics import *
 from or_gym.algos.math_prog_utils import *
+import gym
+import or_gym
 import sys
 from argparse import ArgumentParser
 
@@ -12,14 +15,18 @@ def parse_arguments():
 
 	return parser.parse_args()
 
-def optimize_ukp():
+def optimize_ukp(env):
 
-	model = ukp_math_prog.build_ukp_ip_model('Knapsack-v0')
+	model = build_ukp_ip_model(env)
 	model, results = solve_math_program(model)
 	return model, results
 
 if __name__ == '__main__':
 	# parser = parse_arguments()
 	# args = parser(sys.argv)
-	model, results = optimize_ukp()
-	print("Total reward = {}".format(model.obj.expr()))
+	env = gym.make('Knapsack-v0')
+	model, results = optimize_ukp(env)
+	print("Optimal reward\t\t=\t{}".format(model.obj.expr()))
+	actions, rewards = ukp_heuristic(env)
+	print("Heuristic reward\t=\t{}".format(sum(rewards)))
+	# print("RL reward\t\t=\t{}".format())
