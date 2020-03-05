dab9b68aaa19e88a7a7d8d5ba4ff92ad98611a9a
hubbs5 <christiandhubbs@gmail.com>
2020-03-05 15:59:15 -0600

    adding vmpacking env

---
+++ or_gym/envs/classic_or/vmpacking.py
@@ -0,0 +1,85 @@
+import numpy as np
+import gym
+from gym import spaces, logger
+from gym.utils import seeding
+import copy
+
+class VMPackingEnv(gym.Env):
+	'''
+    Online VM Packing Problem
+
+    The VM Packing Problem (VMPP) is a combinatorial optimization problem which
+    requires the user to select from a series of physical machines (PM's) to
+    send a virtual machine process to. Each VM process is characterized by
+    two values, the memory and compute of the process. These are normalized
+    by the PM capacities to range between 0-1. 
+
+    Observation:
+        Type: Tuple, Discrete
+        [0][:, 0]: Binary indicator for open PM's
+        [0][:, 1]: CPU load of PM's
+        [0][:, 2]: Memory load of PM's
+        [1][0]: Current CPU demand
+        [1][1]: Current memory demand
+
+    Actions:
+        Type: Discrete
+        Integer of PM number to send VM to that PM
+
+    Reward:
+        Negative of the waste, which is the difference between the current
+        size and excess space on the PM.
+
+    Starting State:
+        No open PM's and random starting item
+        
+    Episode Termination:
+        When invalid action is selected, attempt to overload VM, or step
+        limit is reached.
+    '''
+    
+    def __init__(self):
+        # Normalized Capacities
+        self.cpu_capacity = 1
+        self.ram_capacity = 1
+        self.step_limit = 60 * 24 / 15
+        self.n_pms = 100 # Number of physical machines to choose from
+        
+        self.observation_space = spaces.Box(
+            low=np.zeros((self.n_pms, 3), dtype=np.float32),
+            high=np.ones((self.n_pms, 3), dtype=np.float32),
+            dtype=np.float32)
+        self.action_space = spaces.Discrete(self.n_pms)
+        
+        self.state = self.reset()
+        
+    def step(self, action):
+        done = False
+        pm_state = self.state[0]
+        if action < 0 or action >= self.n_pms:
+            raise ValueError('Invalid Action')
+        elif any(pm_state[action, 0] + self.demand[self.step_count] > 1):
+            # Demand doesn't fit into PM
+            reward = -100
+            done = True
+        else:
+            if pm_state[action, 0] == 0:
+                # Open PM if closed
+                pm_state[action, 0] = 1
+            pm_state[action, np.array([1, 2])] += self.demand[self.step_count]
+            reward = np.sum(pm_state[:, 0] * 
+                (pm_state[:, 1] - 1 + pm_state[:, 2] - 1))
+            
+        self.step_count += 1
+        if self.step_count >= self.step_limit:
+            done = True
+            reward = 0
+        self.state = (pm_state, self.demand[self.step_count])
+        
+        return self.state, reward, done, {}
+    
+    def reset(self):
+        self.step_count = 0
+        self.demand = generate_demand()
+        self.state = (np.zeros((self.n_pms, 3)), self.demand[0])
+        return self.state
