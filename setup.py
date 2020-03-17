31a10bbc919e9e4340b402a7e46c6cf401466a02
Christian <christiandhubbs@gmail.com>
2020-03-17 11:55:48 -0500

    adding required installation packages

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(name='or-gym',
 	license='MIT',
 	# pacakges=[package for package in find_packages() if package.startswith('something')],
 	install_requires=['gym>=0.15.0','matplotlib>=3.0.3', 'str2bool>=1',
-		'numpy>=1.16.1','scipy>=1.4.1','torch>=1.4.0','torchvision>=0.5.0','pyomo>=5.5'],
+		'numpy>=1.16.1','scipy>=1.4.1','torch>=1.4.0','torchvision>=0.5.0','pyomo>=5.5',
+		'ray>=0.8', 'pandas>=1.0'],
 	zip_safe=False
 )
