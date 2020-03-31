3840a4c5369427607fe130e1697355ed6e388fb1
Christian <christiandhubbs@gmail.com>
2020-03-31 13:27:03 -0500

    updated newsvendor name

--- or_gym/envs/__init__.py
+++ or_gym/envs/__init__.py
@@ -24,6 +24,6 @@ register(id='VMPacking-v1',
 	entry_point='or_gym.envs.classic_or:TempVMPackingEnv'
 )
 
-register(id='NewsVendor-v0',
+register(id='NewsVendor-v1',
 	entry_point='or_gym.envs.classic_or:MultiLevelNewsVendorEnv'
 )
