5f55d2fb5e8ac96d7cf654c03a34ea192f34455a
Christian <christiandhubbs@gmail.com>
2020-05-08 18:38:37 -0500

    changed action number types

--- or_gym/envs/supply_chain/scheduling.py
+++ or_gym/envs/supply_chain/scheduling.py
@@ -11,10 +11,16 @@ class BaseSchedEnv(gym.Env):
         self.step_limit = 10
         self.transition_prob = [0.5, 0.5]
         self.sales_price = np.ones(self.num_products)
+        self.holding_costs = np.ones(self.num_products) * 2
         self.production_rate = np.ones(self.num_products) * 10
+        self.max_inventory = 100
         self.action_setting = 'discrete'
         assign_env_config(self, kwargs)
-        self.transition_matrix = self.generate_transition_matrix()
+        # self.transition_matrix = self.generate_transition_matrix()
+        self.transition_matrix = np.array([
+            [1, 0, 1],
+            [0, 1, 1],
+            [1, 1, 1]])
         self.obs_dim = self.num_products * 2 + 3
         # Reference to see which columns in state corresponds to different inputs
         self.state_indices = {
@@ -25,17 +31,23 @@ class BaseSchedEnv(gym.Env):
                  for i, j in enumerate(np.arange(self.num_products), 3)},
             'demand': 
                 {'product_' + str(j): i 
-                 for i, j in enumerate(np.arange(self.num_products), self.num_products + 3)}
+                 for i, j in enumerate(np.arange(self.num_products), 
+                    self.num_products + 3)}
         }
         
         self.reset()
 
     def _split_action(self, action):
         if self.action_setting == 'continuous':
-            product, rate = np.argmax(action), np.max(action)
+            # If multiple actions are tied for max value, randomly break tie
+            rate = np.max(action)
+            if len(np.where(action==rate)[0]) > 1:
+                product = np.random.choice(np.where(action==rate)[0])
+            else:
+                product = np.argmax(action)
         else:
             product = int(action)
-            rate = self.production_rate[product]
+            rate = 1 #self.production_rate[product]
         return product, rate
         
     def step(self, action):
@@ -44,15 +56,21 @@ class BaseSchedEnv(gym.Env):
         t = self.current_step
         self.inventory[self.last_action[0]] += self.planned_production
         # To be carried forward to next step
-        self.planned_production = self.transition_matrix[self.last_action[0], product] * rate
+        valid_action = self.transition_matrix[self.last_action[0], product]
+        self.planned_production = valid_action * rate * self.production_rate[product]
+        # Large negative reward for transitions with 0
+        reward = -100 if valid_action == 0 else 0
         
         # Sell products
         sales_qty = [self.demand[t, i] if j else self.inventory[i] 
             for i, j in enumerate(self.inventory>=self.demand[t])]
         self.inventory = np.where(self.inventory - self.demand[t]>=0, 
             self.inventory-self.demand[t], 0)
+        self.inventory = np.where(self.inventory>self.max_inventory, 
+            self.max_inventory, self.inventory)
         self.last_action = [product, rate]
-        reward = np.dot(sales_qty, self.sales_price)
+        reward += np.dot(sales_qty, self.sales_price) - \
+            np.dot(self.inventory, self.holding_costs)
         
         self.current_step += 1
         if self.current_step >= self.step_limit:
@@ -75,17 +93,6 @@ class BaseSchedEnv(gym.Env):
         self.current_step = 0
                 
         return self._update_state()
-    
-    def _update_state(self):
-        t = self.current_step
-        self.state = np.hstack([t, self.last_action, self.inventory, self.demand[t]])
-        idx = self.last_action[0]
-        action_mask = self.transition_matrix[idx]
-        self.state = {'action_mask': action_mask,
-                      'avail_actions': np.ones(self.num_products),
-                      'state': state}
-            
-        return self.state
     
     def generate_demand(self):
         demand = np.zeros((self.step_limit+1, self.num_products))
@@ -117,19 +124,16 @@ class DiscreteSchedEnv(BaseSchedEnv):
     def __init__(self, *args, **kwargs):
         super().__init__()
         self.action_setting = 'discrete'
-        self.action_space = spaces.Discrete(self.num_products)
-        self.observation_space = spaces.Box(0, 100, shape=(self.obs_dim,))
+        self.action_space = spaces.Discrete(self.num_products, dtype=np.int16)
+        self.observation_space = spaces.Box(0, 
+            self.max_inventory, shape=(self.obs_dim,), dtype=np.int16)
 
         self.reset()
 
     def _update_state(self):
         t = self.current_step
-        state = np.hstack([t, self.last_action, self.inventory, self.demand[t]])
-        idx = self.last_action[0]
-        action_mask = self.transition_matrix[idx]
-        self.state = {'action_mask': action_mask,
-                      'avail_actions': np.ones(self.num_products),
-                      'state': state}
+        self.state = np.hstack([t, self.last_action, self.inventory, 
+            self.demand[t]])
             
         return self.state
 
@@ -138,18 +142,23 @@ class MaskedDiscreteSchedEnv(BaseSchedEnv):
     def __init__(self, *args, **kwargs):
         super().__init__()
         self.action_setting = 'discrete'
-        self.action_space = spaces.Discrete(self.num_products)
+        self.action_space = spaces.Discrete(self.num_products, dtype=np.int16)
         self.observation_space = spaces.Dict({
-            'action_mask': spaces.Box(0, 1, shape=(self.num_products,)),
-            'avail_actions': spaces.Box(0, 1, shape=(self.num_products,)),
-            'state': spaces.Box(0, 100, shape=(self.obs_dim,))
+            'action_mask': spaces.Box(0, 1, shape=(self.num_products,), 
+                dtype=np.int16),
+            'avail_actions': spaces.Box(0, 1, shape=(self.num_products,), 
+                dtype=np.int16),
+            'state': spaces.Box(0, 
+                self.max_inventory, shape=(self.obs_dim,),
+                dtype=np.float32)
         })
 
         self.reset()
 
     def _update_state(self):
         t = self.current_step
-        state = np.hstack([t, self.last_action, self.inventory, self.demand[t]])
+        state = np.hstack([t, self.last_action, self.inventory, 
+            self.demand[t]])
         idx = self.last_action[0]
         action_mask = self.transition_matrix[idx]
         self.state = {'action_mask': action_mask,
@@ -164,15 +173,18 @@ class ContSchedEnv(BaseSchedEnv):
         super().__init__()
         self.action_setting = 'continuous'
         self.action_space = spaces.Box(
-            low=np.zeros(self.num_products), 
-            high=self.production_rate)
-        self.observation_space = spaces.Box(0, 100, shape=(self.obs_dim,))
+            low=np.zeros(self.num_products),
+            high=np.ones(self.num_products), 
+            dtype=np.float32)
+        self.observation_space = spaces.Box(0, self.max_inventory, 
+            shape=(self.obs_dim,), dtype=np.float32)
 
         self.reset()            
 
     def _update_state(self):
         t = self.current_step
-        self.state = np.hstack([t, self.last_action, self.inventory, self.demand[t]])            
+        self.state = np.hstack([t, self.last_action, 
+            self.inventory, self.demand[t]])            
         return self.state
 
 class MaskedContSchedEnv(BaseSchedEnv):
@@ -182,18 +194,23 @@ class MaskedContSchedEnv(BaseSchedEnv):
         self.action_setting = 'continuous'
         self.action_space = spaces.Box(
             low=np.zeros(self.num_products), 
-            high=self.production_rate)
+            high=np.ones(self.num_products), 
+            dtype=np.float32)
         self.observation_space = spaces.Dict({
-            'action_mask': spaces.Box(0, 1, shape=(self.num_products,)),
-            'avail_actions': spaces.Box(0, 1, shape=(self.num_products,)),
-            'state': spaces.Box(0, 100, shape=(self.obs_dim,))
+            'action_mask': spaces.Box(0, 1, shape=(self.num_products,), 
+                dtype=np.int16),
+            'avail_actions': spaces.Box(0, 1, shape=(self.num_products,),
+                dtype=np.int16),
+            'state': spaces.Box(0, self.max_inventory, shape=(self.obs_dim,),
+                dtype=np.float32)
         })
 
         self.reset()
 
     def _update_state(self):
         t = self.current_step
-        state = np.hstack([t, self.last_action, self.inventory, self.demand[t]])
+        state = np.hstack([t, self.last_action, 
+            self.inventory, self.demand[t]])
         idx = self.last_action[0]
         action_mask = self.transition_matrix[idx]
         self.state = {'action_mask': action_mask,
