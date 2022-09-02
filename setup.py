ad5d0fd1cbfbeceb0c74f0a5224fc335d244aaf0
osarwar <owais.sarwar@gmail.com>
2022-09-02 11:50:48 -0500

    changed python version to 3.7

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(name='or-gym',
 		'matplotlib>=3.1',
 		'networkx>=2.3'],
 	zip_safe=False,
-	python_requires='>=3.6',
+	python_requires='>=3.7',
 	classifiers=[
 		'Development Status :: 3 - Alpha',
 		'Intended Audience :: Developers',
